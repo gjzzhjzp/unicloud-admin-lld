c54e3ef9c354ec33aba518ee7527d93c11ac06b6
兰云杉 <872610221@qq.com>
2022-02-12 09:55:42 +0800

    1

--- pages/jz-opendb-resourceshj/list.vue
+++ pages/jz-opendb-resourceshj/list.vue
@@ -26,7 +26,6 @@
 				<uni-table ref="table" :loading="loading" :emptyText="error.message || '没有更多数据'" border stripe
 					type="selection" @selection-change="selectionChange">
 					<uni-tr>
-						
 						<uni-th align="center" filter-type="search"
 							@filter-change="filterChange($event, 'resourceshj_title')" sortable
 							@sort-change="sortChange($event, 'resourceshj_title')">合集标题</uni-th>
@@ -38,7 +37,8 @@
 							<view class="uni-group">
 								<button @click="navigateTo('./edit?id='+item._id, false)" class="uni-button" size="mini"
 									type="primary">修改</button>
-								<button @click="openhjgl(item.hj_id)" class="uni-button" size="mini" type="warn">合集管理</button>
+								<button @click="openhjgl(item.hj_id)" class="uni-button" size="mini"
+									type="warn">合集管理</button>
 								<button @click="confirmDelete(item._id)" class="uni-button" size="mini"
 									type="warn">删除</button>
 							</view>
@@ -61,20 +61,23 @@
 			<u-modal v-model="showaddhjzy" width="600px" @cancel="showaddhjzy==false" :show-cancel-button="true"
 				title="新增合集资源" @confirm="confirmaddhjzy">
 				<view class="uni-container" style="height: 500px;overflow: auto;">
-					<unicloud-db ref="listudb" collection="jz-opendb-resources" field="title,zy_gs" :where="listwhere"
-						page-data="replace" orderby="publish_date desc" :getcount="true" :page-size="100"
-						:page-current="options.pageCurrent" v-slot:default="{data,pagination,loading,error,options}"
-						:options="options" loadtime="manual">
+					<unicloud-db ref="listudb" collection="jz-opendb-resources" field="title,zy_gs,author"
+						:where="listwhere" page-data="replace" orderby="publish_date desc" :getcount="true"
+						:page-size="100" :page-current="options.pageCurrent"
+						v-slot:default="{data,pagination,loading,error,options}" :options="options" loadtime="manual">
 						<uni-table ref="listtable" :loading="loading" :emptyText="error.message || '没有更多数据'" border
 							stripe type="selection" @selection-change="listselectionChange">
 							<uni-tr>
 								<uni-th align="center" filter-type="search"
 									@filter-change="listfilterChange($event, 'title')">标题</uni-th>
-									<uni-th align="center" style="width: 100px;">格式</uni-th>
+								<uni-th align="center" width="200" filter-type="search"
+									@filter-change="listfilterChange($event, 'author')">来源</uni-th>
+								<uni-th align="center" style="width: 100px;">格式</uni-th>
 								<uni-th align="center">排序</uni-th>
 							</uni-tr>
 							<uni-tr v-for="(item,index) in data" :key="index">
 								<uni-td align="center">{{item.title}}</uni-td>
+								<uni-td align="center">{{item.author}}</uni-td>
 								<uni-td align="center">{{zy_gs_localdata[item.zy_gs].text}}</uni-td>
 								<uni-td align="center">
 									<view class="uni-group">
@@ -91,20 +94,36 @@
 					</unicloud-db>
 				</view>
 			</u-modal>
-			<u-modal v-model="showhjManage" width="600px" @cancel="showhjManage==false" :show-cancel-button="true"
+			<u-modal v-model="showhjManage" width="800px" @cancel="showhjManage==false" :show-cancel-button="true"
 				title="合集管理" @confirm="addhjzy" confirm-text="添加">
+				<view style="color: red;padding: 0px 10px;">
+					提示：右击排序号修改，再次右击保存
+				</view>
 				<view class="uni-container" style="height: 500px;overflow: auto;">
-					<uni-table ref="table" emptyText="'没有更多数据'" border stripe>
+					<uni-table ref="table1" emptyText="'没有更多数据'" border stripe>
 						<uni-tr>
 							<uni-th align="center">资源标题</uni-th>
+							<uni-th align="center">来源</uni-th>
 							<uni-th align="center">格式</uni-th>
-							<uni-th align="center">排序</uni-th>
+							<uni-th align="center" style="width: 100px;">排序</uni-th>
 							<uni-th align="center">操作</uni-th>
 						</uni-tr>
 						<uni-tr v-for="(item,index) in hjdata" :key="index">
 							<uni-td align="center">{{item.article_id[0]?item.article_id[0].title:''}}</uni-td>
-							<uni-td align="center">{{zy_gs_localdata[item.article_id[0]?item.article_id[0].zy_gs:''].text}}</uni-td>
-							<uni-td align="center">{{item.sort}}</uni-td>
+							<uni-td align="center">{{item.article_id[0]?item.article_id[0].author:''}}</uni-td>
+							<uni-td align="center">
+								{{zy_gs_localdata[item.article_id[0]?item.article_id[0].zy_gs:''].text}}
+							</uni-td>
+							<uni-td align="center" >
+							<view class="px" @contextmenu.stop.prevent="changepx(item)">
+								<uni-easyinput v-if="item.xgpx" placeholder="排序号" type="number" v-model="item.sort">
+								</uni-easyinput>
+								<view v-else>
+									{{item.sort}}
+								</view>
+							</view>
+							
+							</uni-td>
 							<uni-td align="center">
 								<view class="uni-group">
 									<button @click="deletehjzy(item._id)" class="uni-button" size="mini"
@@ -140,6 +159,7 @@
 	export default {
 		data() {
 			return {
+				xgpx:false,
 				"zy_gs_localdata": [{
 						"value": 0,
 						"text": "图片"
@@ -157,8 +177,8 @@
 						"text": "文章"
 					}
 				],
-				curhjid:"",///当前合集id
-				showaddhjzy:false,
+				curhjid: "", ///当前合集id
+				showaddhjzy: false,
 				hjdata: [],
 				showhjManage: false,
 				collectionList: "jz-opendb-resourceshj,jz-opendb-resources",
@@ -200,19 +220,35 @@
 			this.$refs.udb.loadData()
 		},
 		methods: {
+			async changepx(item){
+				
+				this.$set(item,"xgpx",!(item.xgpx))
+				if(!item.xgpx){
+					await db.collection("jz-opendb-resourceshj").where({
+						"_id": item._id
+					}).update({
+						sort:parseInt(item.sort)
+					})
+					// debugger;
+					this.openhjgl(item.parent_id);
+				}
+				// item.xgpx=!(item.xgpx);
+			},
 			addhjzy() {
 				this.showaddhjzy = true;
 			},
 			async openhjgl(hj_id) {
 				this.showhjManage = true;
-				this.curhjid=hj_id;
+				this.curhjid = hj_id;
 				// 获取合集数据
 				var res = await db.collection("jz-opendb-resourceshj,jz-opendb-resources").where({
 					"parent_id": hj_id
-				}).field("resourceshj_title,sort,article_id{title,zy_gs},hj_id").get();
-				console.log("res", res);
+				}).field("resourceshj_title,parent_id,sort,article_id{title,zy_gs,author},hj_id").orderBy("sort asc").get();
+				console.log("合集数据", res);
 				if (res.result && res.result.data && res.result.data.length > 0) {
 					this.hjdata = res.result.data;
+				}else{
+					this.hjdata = [];
 				}
 			},
 			// 多选处理
@@ -220,13 +256,13 @@
 				// debugger;
 				var list = [];
 				var dataList = this.$refs.listudb.dataList;
-				var sortinit=this.hjdata.length-1||0;
+				// var sortinit = this.hjdata.length - 1 || 0;
 				dataList.forEach((item, index) => {
 					if (this.selectedIndexs.indexOf(index) != -1) {
 						list.push({
 							parent_id: hj_id,
 							hj_id: hj_id + index,
-							sort: parseInt(item.sort) || ++sortinit,
+							sort: parseInt(item.sort) || 0,
 							article_id: item._id
 						});
 					}
@@ -253,15 +289,16 @@
 				this.showaddhj = false;
 				await db.collection("jz-opendb-resourceshj").add({
 					hj_id,
-					resourceshj_title:this.addtitle
+					resourceshj_title: this.addtitle
 				});
 				this.$refs.udb.loadData({
 					current: 1
 				});
+				this.addtitle="";
 				uni.hideLoading();
 			},
 			async confirmaddhjzy() {
-				this.showhjManage=true;
+				this.showhjManage = true;
 				this.showaddhjzy = true;
 				var lists = this.listselectedItems(this.curhjid);
 				if (lists.length == 0) {
@@ -285,7 +322,7 @@
 			},
 			listonPageChanged(e) {
 				this.listselectedIndexs.length = 0;
-				if(this.$refs.listtable){
+				if (this.$refs.listtable) {
 					this.$refs.listtable.clearSelection()
 				}
 				this.$refs.listudb.loadData({
@@ -304,9 +341,9 @@
 				} else {
 					this.listwhere = ''
 				}
-				Object.assign(this.listwhere,{
-					is_off:db.command.neq(1),
-					article_status:db.command.neq(0)
+				Object.assign(this.listwhere, {
+					is_off: db.command.neq(1),
+					article_status: db.command.neq(0)
 				})
 				this.$nextTick(() => {
 					this.$refs.listudb.loadData()
@@ -380,14 +417,18 @@
 				this.selectedIndexs = e.detail.index
 			},
 			confirmDelete(id) {
+				// debugger;
 				this.$refs.udb.remove(id, {
 					success: (res) => {
-						this.$refs.table.clearSelection()
+						// debugger;
+						if(this.$refs.table){
+							this.$refs.table.clearSelection()
+						}
 					}
 				})
 			},
 			async deletehjzy(id) {
-				var that=this;
+				var that = this;
 				uni.showModal({
 					title: '提示',
 					content: '确认删除吗？',
@@ -402,7 +443,7 @@
 							that.openhjgl(that.curhjid);
 							uni.hideLoading();
 						} else if (res.cancel) {
-				   console.log('用户点击取消');
+							console.log('用户点击取消');
 						}
 					}
 				});
